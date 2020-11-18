bd4fc69fd16519e592dc17e8ce65f76e066762aa
Garrett Hopkins <garretth@thedomain.ava.ai>
2020-11-17 18:27:46 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// // import logo from './logo.svg';
+ import React from 'react';
+import Header from './components/Header/Header'
+// // import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+ import LoginButton from './components/LoginButton/LoginButton';
+ import LogoutButton from './components/LogoutButton/LogoutButton';
+ import Profile from "./components/Profile/Profile";
+
+
+ import './App.css';
+
+ function App() {
+   return (
+     <div className="App">
+      < Header />
+      <LoginButton /> 
+       <LogoutButton />
+      <Profile />
+     </div>
+   )
+ }
+
+ export default App;
+
+
