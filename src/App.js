2ee06c505d0b7d7eed702d61b27e4515df7bd13b
Garrett Hopkins <garretth@thedomain.ava.ai>
2020-11-18 13:44:17 -0800

    my commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ import Header from './components/Header/Header'
  function App() {
    return (
      <div className="App">
-      < Header />
       <LoginButton /> 
-       <LogoutButton />
+      <LogoutButton />
       <Profile />
      </div>
    )
